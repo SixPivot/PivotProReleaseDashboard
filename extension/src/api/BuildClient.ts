7237cc1232bd6f8a8d8b404167032c144bf24f7c
David Gardiner <david@gardiner.net.au>
2025-06-15 20:28:32 +0930

    Get latest build name

--- extension/src/api/BuildClient.ts
+++ extension/src/api/BuildClient.ts
@@ -1,13 +1,13 @@
-import { getClient } from 'azure-devops-extension-api';
-import { BuildRestClient } from 'azure-devops-extension-api/Build';
+import { getClient } from 'azure-devops-extension-api'
+import { BuildRestClient } from 'azure-devops-extension-api/Build'
 
 export async function getBuildName(project: string, buildId: number): Promise<string | undefined> {
-    const buildClient = getClient(BuildRestClient);
+    const buildClient = getClient(BuildRestClient)
     try {
-        const build = await buildClient.getBuild(project, buildId);
-        return build.name;
+        const build = await buildClient.getBuild(project, buildId)
+        return build.buildNumber
     } catch (e) {
-        // Could not fetch build, return the build ID as a fallback
-        return buildId.toString();
+        // Could not fetch build
+        return undefined
     }
 }
